f69f34a9ac6edf224de24cf2dcaa4abe6e0767d6
thanhphongcao1601 <thanhphongcao1601@gmail.com>
2022-12-09 09:50:11 +0700

    header #1

--- tiktok-ui/src/components/Layout/components/Header/index.tsx
+++ tiktok-ui/src/components/Layout/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 
@@ -7,7 +9,21 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/TikTok_logo.svg/1200px-TikTok_logo.svg.png"
+          alt="No image"
+        />
+        <div className={cx('search')}>
+          <input type="text" placeholder="Search acounts and videos" />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className="action"></div>
       </div>
     </header>
   );
